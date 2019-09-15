90f4df1f9f3f2d97ad9207c62001014ad0ac8e1c
Alexander Kosyakov <>
2019-09-15 22:44:34 +0300

    add cli.js

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-exports.printMsg = function() {
-  console.log("This is a message from the demo package");
-}
+const yargs = require("yargs");
+
+const options = yargs
+ .usage("Usage: -n <name>")
+ .option("n", { alias: "name", describe: "Your name", type: "string", demandOption: true })
+ .option("q", { alias: "second name", describe: "Your second name", type: "string", demandOption: true })
+ .option("w", { alias: "last name", describe: "Your last name", type: "string", demandOption: true })
+ .argv;
+
+const greeting = `Hello, ${options.name}!`;
+
+console.log(greeting);
